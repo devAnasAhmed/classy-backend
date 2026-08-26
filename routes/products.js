244ad02b13db92f43cd035652236bb361d625b56
devAnasAhmed <devanasahmed@gmail.com>
2026-08-26 18:27:51 +0300

    Update products.js

--- routes/products.js
+++ routes/products.js
@@ -1,13 +1,47 @@
 const express = require('express');
 const router = express.Router();
-const { getAll, getOne, create, update, delete: deleteProduct } = require('../controllers/productController');
+
+const {
+  getAll,
+  getOne,
+  create,
+  update,
+  delete: deleteProduct
+} = require('../controllers/productController');
+
 const { protect, adminOnly } = require('../middleware/auth');
 const upload = require('../middleware/upload');
 
+// Get all products
 router.get('/', getAll);
+
+// Get one product
 router.get('/:id', getOne);
-router.post('/', protect, adminOnly, upload.single('image'), create);
-router.put('/:id', protect, adminOnly, upload.single('image'), update);
-router.delete('/:id', protect, adminOnly, deleteProduct);
+
+// Create product
+router.post(
+  '/',
+  protect,
+  adminOnly,
+  upload.single('image'),
+  create
+);
+
+// Update product
+router.put(
+  '/:id',
+  protect,
+  adminOnly,
+  upload.single('image'),
+  update
+);
+
+// Delete product
+router.delete(
+  '/:id',
+  protect,
+  adminOnly,
+  deleteProduct
+);
 
 module.exports = router;
